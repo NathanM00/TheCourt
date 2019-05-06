2769f6769d890e9b657e4f087f4ec1d1bc348641
Nathan M <nathanmasol@gmail.com>
2019-05-06 09:06:22 -0500

    Pagina de producto inicial, agrega a carrito

--- index.js
+++ index.js
@@ -31,28 +31,36 @@ app.engine('handlebars',exphbs());
 //Establecer handlebars como el motor de render
 app.set('view engine','handlebars');
 
-//Arreglo de productos
-var productos = [];
-productos.push({
-    titulo: 'aaaa',
-    precio: 20,
-    imagen: "../public/recursos/productosA1.jpeg",
-    descripcion: "Your kiddo won't be able to contain their excitement when you gift them this Boston Celtics Essential logo hoodie from Nike. The cold won't stand a chance when your young one has this warm hoodie on." 
-});
-
 //Ruta inicial
 app.get('/',function(req,res){
     res.sendFile(__dirname+'/public/index.html')
 });
 
+app.get('/tienda/producto/:nombre', function(req, res){
+
+    var collection = db.collection('productos');
+    collection.find({ nombre: req.params.nombre })
+        .toArray(function(err, docs){
+            console.log(docs);
+
+            var contexto = {
+                producto: docs[0]
+            };
+            res.render('producto', contexto);
+        });
+});
+
 //ruta dinamica
 app.get('/tienda/:departamento?', function(request, response){
     
-    console.log(request.query.precio);
+    //console.log(request.query.precio);
 
     var query = {};
     if(request.params.departamento){
         query.departamento = request.params.departamento;
+    }
+    if(request.params.linea){
+        query.linea = request.params.linea;
     }
     if(request.query.precio){
         query.precio = { $lte: request.query.precio };
@@ -65,11 +73,9 @@ app.get('/tienda/:departamento?', function(request, response){
         
         var contexto = {
             productos: docs,
+            linea: request.params.linea,
             departamento: request.params.departamento,
             precio: request.query.precio,
-            esCamiseta: request.params.departamento == "T-shirts",
-            esSaco: request.params.departamento == "Hoodies & Sweatshirts",
-            esJersey: request.params.departamento == "Jerseys",
         };
         response.render('tienda', contexto);
     });
---
+++ public/scripts/javas1.js
@@ -0,0 +1,67 @@
+function paginaCargada(){
+
+  var rango = document.querySelector('.rangoprecio');
+
+      function buscarPorPrecio(){
+          location.href = '/tienda/precio=' + rango.value;
+      }
+      rango.addEventListener('change', buscarPorPrecio);
+
+      var listaProductos = [];
+      if(localStorage.getItem('listaProductos') != null){
+          listaProductos = JSON.parse(localStorage.getItem('listaProductos'));
+      }
+  
+      var carritoNum = document.querySelector('.encabezado__contador');
+      var listaCarrito = document.querySelector('.carrito-desplegado__lista');
+  
+      function actualizarCarrito(){
+          carritoNum.innerHTML = listaProductos.length;
+      }
+  
+      actualizarCarrito();
+  
+      var botones = document.querySelectorAll('.tienda__agregar');
+      function recorrerBotones(boton){
+          function agregarAlCarrito(){
+              var padre = boton.parentNode;
+              var nombre = padre.querySelector('.item__nombre').innerText;
+              var precio = padre.querySelector('.item__precio').innerText;
+              var imagen = padre.querySelector('.item__fotoP').src;
+              var producto = {
+                  nombre: nombre,
+                  precio: precio,
+                  imagen: imagen,
+              };
+              
+              listaProductos.push(producto);
+              actualizarCarrito();
+              localStorage.setItem('listaProductos', JSON.stringify(listaProductos));
+          }
+          boton.addEventListener('click', agregarAlCarrito);
+      }
+      botones.forEach(recorrerBotones);
+  
+  
+      var botonProductoDetalle = document.querySelector('.producto-detalle__carrito');
+      function agregarAlCarritoDetalle(){
+          var nombre = document.querySelector('.item__nombre').innerText;
+          var precio = document.querySelector('.item__precio').innerText;
+          var imagen = document.querySelector('.item__fotoP').src;
+          var producto = {
+              nombre: nombre,
+              precio: precio,
+              imagen: imagen,
+          };
+          
+          listaProductos.push(producto);
+          carritoNum.innerHTML = listaProductos.length;
+          localStorage.setItem('listaProductos', JSON.stringify(listaProductos));
+      }
+      if(botonProductoDetalle != null){
+          botonProductoDetalle.addEventListener('click', agregarAlCarritoDetalle);
+      }
+
+}
+
+window.addEventListener('load', paginaCargada);
